d057357db409712aa891125cf20a999a30afddd8
Ben Wiedeback <66647604+Inlic@users.noreply.github.com>
2020-12-02 16:42:22 -0700

    updated components to classes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+import React, { Component } from "react";
+import Todos from './components/Todos';
+import './App.css';
+
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      }
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <Todos />
+      </div>
+    );
+  }
 }
 
 export default App;
