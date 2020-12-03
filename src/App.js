45f1fe07a9cb2a4566e54a031087385809096f7e
Ben Wiedeback <66647604+Inlic@users.noreply.github.com>
2020-12-03 16:43:30 -0700

    add header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import './App.css';
 
@@ -35,6 +36,7 @@ class App extends Component {
       })
     });
   }
+  // Delete Todo
   delTodo = (id) => {
     this.setState({
       todos: [...this.state.todos.filter(t => t.id != id)]
@@ -44,6 +46,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
       </div>
     );
