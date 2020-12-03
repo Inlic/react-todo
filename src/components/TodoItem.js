c76cb598b3acea6571d68ac79f85ad100a3167d4
Ben Wiedeback <66647604+Inlic@users.noreply.github.com>
2020-12-03 16:38:18 -0700

    delete todos front end

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -15,8 +15,11 @@ export class TodoItem extends Component {
     const { id, title } = this.props.todo
     return (
       <div style={this.getStyle()}>
-        <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} />{' '}
-        <p>{title}</p>
+        <p>
+          <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} />{' '}
+          {title}
+          <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
+        </p>
       </div>
     )
   }
@@ -24,6 +27,16 @@ export class TodoItem extends Component {
 
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired
+}
+
+const btnStyle = {
+  background: '#ff0000',
+  color: '#fff',
+  border: 'none',
+  padding: '5px 10px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right'
 }
 
 export default TodoItem
